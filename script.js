e645f6c7d72b3b9e0ae1e8a687b5a4bb06c1b7b6
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-02-23 06:22:57 -0600

    completed

---
+++ script.js
@@ -0,0 +1,16 @@
+// TODO: What will the following line of code log?
+console.log(window); 
+// [window object]
+
+// TODO: What will the following line of code log?
+console.log(window.document); 
+// [the document]
+
+// TODO: What will the following line of code log?
+console.log(document.documentElement);
+// [elements of the document documentElement]
+
+// TODO: What will the following line of code log?
+console.log(document.head); 
+// [document head]
+
